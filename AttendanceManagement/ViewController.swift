248965b14f664be81e27a0378377261e1bbd33d2
Binsent7 <hishinuma.kougakuin@gmail.com>
2016-02-21 22:11:45 +0900

    Initial Commit

---
+++ AttendanceManagement/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AttendanceManagement
+//
+//  Created by hishinuma on 2016/02/21.
+//  Copyright © 2016年 Plus. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
