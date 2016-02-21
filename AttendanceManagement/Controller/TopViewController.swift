83610f8a9b25aa9d7d824358b09a3d0e1cc299cc
Binsent7 <hishinuma.kougakuin@gmail.com>
2016-02-21 22:29:18 +0900

    プロジェクト階層を整理

--- AttendanceManagement/Controller/TopViewController.swift
+++ AttendanceManagement/Controller/TopViewController.swift
@@ -1,25 +1,26 @@
 //
-//  ViewController.swift
+//  TopViewController.swift
 //  AttendanceManagement
 //
 //  Created by hishinuma on 2016/02/21.
 //  Copyright © 2016年 Plus. All rights reserved.
 //
 
+import Foundation
+
 import UIKit
 
-class ViewController: UIViewController {
-
+class TopViewController: UIViewController {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
-
