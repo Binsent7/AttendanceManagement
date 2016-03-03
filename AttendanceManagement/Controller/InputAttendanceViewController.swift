068798555f572f4c0c5ee1848482c44f86884844
Binsent7 <hishinuma.kougakuin@gmail.com>
2016-03-04 01:05:04 +0900

    種別選択画面を追加

--- AttendanceManagement/Controller/InputAttendanceViewController.swift
+++ AttendanceManagement/Controller/InputAttendanceViewController.swift
@@ -11,8 +11,6 @@ import UIKit
 
 class InputAttendanceViewController: UIViewController {
     
-    
-    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
@@ -34,21 +32,70 @@ class InputAttendanceViewController: UIViewController {
     @IBAction func onTapCancelButton(sender: AnyObject) {
         dismissViewControllerAnimated(true, completion: nil)
     }
+    
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject!) {
+//        if (segue.identifier == "ShowSelectType") {
+//            navigationController?.pushViewController(SelectTypeViewController(), animated: true)
+//        }
+    }
 }
 
 
 extension InputAttendanceViewController: UITableViewDataSource {
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 5
+    }
+    
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return 1
     }
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = UITableViewCell(style: .Default, reuseIdentifier: "cell")
+        if indexPath.section == 0 {
+            cell.accessoryType = UITableViewCellAccessoryType.DisclosureIndicator
+        }
         
         return cell
+    }
+    
+    func tableView(tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        switch section {
+        case 0:
+            return "種別"
+        case 1:
+            return "開始時間"
+        case 2:
+            return "終了時間"
+        case 3:
+            return "休憩時間"
+        case 4:
+            return "その他"
+        default:
+            return nil
+        }
     }
 }
 
 extension InputAttendanceViewController: UITableViewDelegate {
-    
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        // セルの選択状態を解除
+        tableView.deselectRowAtIndexPath(indexPath, animated: true)
+        
+        switch indexPath.section {
+        case 0:
+            print("種別")
+            performSegueWithIdentifier("ShowSelectType", sender: nil)
+        case 1:
+            print("開始時間")
+        case 2:
+            print("終了時間")
+        case 3:
+            print("休憩時間")
+        case 4:
+            print("その他")
+        default:
+            break
+        }
+    }
 }
---
+++ AttendanceManagement/Controller/SelectTypeViewController.swift
@@ -0,0 +1,51 @@
+//
+//  SelectTypeViewController.swift
+//  AttendanceManagement
+//
+//  Created by hishinuma on 2016/03/04.
+//  Copyright © 2016年 Plus. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class SelectTypeViewController: UIViewController {
+
+    private let typeList: [String] = ["出勤","徹夜","徹夜明","出張","欠勤","有休A","有休B","半休A","半休B","振休","特休","遅刻A","遅刻B","早出","休日"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "種別選択"
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+    }
+}
+
+extension SelectTypeViewController: UITableViewDataSource {
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return typeList.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: .Default, reuseIdentifier: "cell")
+        cell.textLabel?.text = typeList[indexPath.row]
+        return cell
+    }
+}
+
+extension SelectTypeViewController: UITableViewDelegate {
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        // セルの選択状態を解除
+        tableView.deselectRowAtIndexPath(indexPath, animated: true)
+        navigationController?.popViewControllerAnimated(true)
+        
+        // FIXME: Realmの情報を更新する
+    }
+}
