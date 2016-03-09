31a03aa4a0d8d8c67664ac3e66db73a325693571
Binsent7 <hishinuma.kougakuin@gmail.com>
2016-03-09 23:00:16 +0900

    勤怠基本情報画面を実装

--- AttendanceManagement/Controller/BasicSettingViewController.swift
+++ AttendanceManagement/Controller/BasicSettingViewController.swift
@@ -8,12 +8,35 @@
 
 import Foundation
 import UIKit
+import RealmSwift
 
 class BasicSettingViewController: UIViewController {
     let sectionTitleList = ["出勤時間","退勤時間","休憩開始時間","休憩終了時間"]
     
+    // FIXME: segue.identifierを定数定義
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    var basicAttendance: BasicAttendance!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let realm = try! Realm()
+        if let basicAttendance = realm.objectForPrimaryKey(BasicAttendance.self, key: "1") {
+            self.basicAttendance = BasicAttendance(value: basicAttendance)
+        }
+        else {
+            basicAttendance = BasicAttendance()
+            basicAttendance.startHour       = 9
+            basicAttendance.startMinute     = 0
+            basicAttendance.endHour         = 18
+            basicAttendance.endMinute       = 0
+            basicAttendance.restStartHour   = 12
+            basicAttendance.restStartMinute = 0
+            basicAttendance.restEndHour     = 13
+            basicAttendance.restEndMinute   = 0
+        }
     }
     
     override func viewWillAppear(animated: Bool) {
@@ -26,15 +49,59 @@ class BasicSettingViewController: UIViewController {
         super.didReceiveMemoryWarning()
     }
     
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        if let viewController = segue.destinationViewController as? HourMinutePickerViewController {
+            if segue.identifier == "ShowBasicSettingStartTime" {
+                // 出勤時間
+                viewController.instanciate(hour: basicAttendance.startHour, minute: basicAttendance.startMinute) { hour, minute in
+                    self.basicAttendance.startHour = hour
+                    self.basicAttendance.startMinute = minute
+                    self.tableView.reloadData()
+                }
+            }
+            else if segue.identifier == "ShowBasicSettingEndTime" {
+                // 退勤時間
+                viewController.instanciate(hour: basicAttendance.endHour, minute: basicAttendance.endMinute) { hour, minute in
+                    self.basicAttendance.endHour = hour
+                    self.basicAttendance.endMinute = minute
+                    self.tableView.reloadData()
+                }
+            }
+            else if segue.identifier == "ShowBasicSettingRestStartTime" {
+                // 休憩開始時間
+                viewController.instanciate(hour: basicAttendance.restStartHour, minute: basicAttendance.restStartMinute) { hour, minute in
+                    self.basicAttendance.restStartHour = hour
+                    self.basicAttendance.restStartMinute = minute
+                    self.tableView.reloadData()
+                }
+            }
+            else if segue.identifier == "ShowBasicSettingRestEndTime" {
+                // 休憩終了時間
+                viewController.instanciate(hour: basicAttendance.restEndHour, minute: basicAttendance.restEndMinute) { hour, minute in
+                    self.basicAttendance.restEndHour = hour
+                    self.basicAttendance.restEndMinute = minute
+                    self.tableView.reloadData()
+                }
+            }
+        }
+    }
+    
     // 保存ボタンタップ
     @IBAction func onTapSaveButon(sender: AnyObject) {
-        // FIXME: 保存する処理
+        saveBasicAttendance()
         close()
     }
     
     // キャンセルボタンタップ
     @IBAction func onTapCancelButton(sender: AnyObject) {
         close()
+    }
+    
+    private func saveBasicAttendance() {
+        let realm = try! Realm()
+        try! realm.write {
+            realm.add(basicAttendance, update: true)
+        }
     }
     
     // 画面を閉じる
@@ -54,12 +121,34 @@ extension BasicSettingViewController: UITableViewDataSource {
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = UITableViewCell(style: .Default, reuseIdentifier: "cell")
-        cell.textLabel?.text = sectionTitleList[indexPath.section]
+        var hour = 0
+        var minute = 0
+        switch indexPath.section {
+        case 0:
+            hour = basicAttendance.startHour
+            minute = basicAttendance.startMinute
+        case 1:
+            hour = basicAttendance.endHour
+            minute = basicAttendance.endMinute
+        case 2:
+            hour = basicAttendance.restStartHour
+            minute = basicAttendance.restStartMinute
+        case 3:
+            hour = basicAttendance.restEndHour
+            minute = basicAttendance.restEndMinute
+        default:
+            break
+        }
+        cell.textLabel?.text = String(format: "%02d : %02d",hour, minute)
         return cell
     }
 }
 
 extension BasicSettingViewController: UITableViewDelegate {
+    func tableView(tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return sectionTitleList[section]
+    }
+    
     func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
         tableView.deselectRowAtIndexPath(indexPath, animated: true)
         
---
+++ AttendanceManagement/Model/BasicAttendance.swift
@@ -0,0 +1,27 @@
+//
+//  BasicAttendance.swift
+//  AttendanceManagement
+//
+//  Created by hishinuma on 2016/03/09.
+//  Copyright © 2016年 Plus. All rights reserved.
+//
+
+import Foundation
+import RealmSwift
+import SwiftDate
+
+class BasicAttendance: Object {
+    dynamic var id              = "1"
+    dynamic var startHour       = 0
+    dynamic var startMinute     = 0
+    dynamic var endHour         = 0
+    dynamic var endMinute       = 0
+    dynamic var restStartHour   = 0
+    dynamic var restStartMinute = 0
+    dynamic var restEndHour     = 0
+    dynamic var restEndMinute   = 0
+    
+    override static func primaryKey() -> String? {
+        return "id"
+    }
+}
